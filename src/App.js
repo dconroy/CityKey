9f288e1ea6672a566110a7d663da1b655b501a90
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2018-03-06 15:54:56 -0500

    fix import error

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 // Frameworks
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import * as AppActions from './actions/AppActions'
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as AppActions from "./actions/AppActions";
 
-import styled from 'styled-components'
+import styled from "styled-components";
 
 // Components
-import AppNavbar from './components/AppNavbar'
-import Welcome from './components/Welcome'
-import SignTransaction from './components/SignTransaction'
-import CollectCredentials from './components/CollectCredentials'
-import RegisterYourApp from './components/RegisterYourApp'
-import LogOut from './components/LogOut'
+import AppNavbar from "./components/AppNavbar";
+import Welcome from "./components/Welcome";
+import AddIdentity from "./components/AddIdentity";
+import CollectCredentials from "./components/CollectCredentials";
+import RegisterYourApp from "./components/RegisterYourApp";
+import LogOut from "./components/LogOut";
 
 const AppWrap = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   height: 100%;
-`
+`;
 
 const AppBody = styled.div`
   flex: 1 100%;
@@ -30,46 +30,32 @@ const AppBody = styled.div`
   text-align: center;
   max-width: 100%;
   padding: 20px;
-`
+`;
 
 class App extends Component {
-  render () {
+  render() {
     return (
       <AppWrap>
         <AppNavbar />
         <AppBody>
-          {
-            !this.props.uport &&
-            !this.props.signTransactionPage
-              ? <Welcome />
-              : null
-          }
-          {
-            this.props.signTransactionPage === true &&
-            !this.props.collectCredentialsPage
-              ? <SignTransaction />
-              : null
-          }
-          {
-            this.props.collectCredentialsPage &&
-            !this.props.registerYourAppPage
-              ? <CollectCredentials />
-              : null
-          }
-          {
-            this.props.registerYourAppPage &&
-            !this.props.logOutPage
-              ? <RegisterYourApp />
-              : null
-          }
-          {
-            this.props.logOutPage
-              ? <LogOut />
-              : null
-          }
+          {!this.props.uport && !this.props.signTransactionPage ? (
+            <Welcome />
+          ) : null}
+          {this.props.signTransactionPage === true &&
+          !this.props.collectCredentialsPage ? (
+            <AddIdentity />
+          ) : null}
+          {this.props.collectCredentialsPage &&
+          !this.props.registerYourAppPage ? (
+            <CollectCredentials />
+          ) : null}
+          {this.props.registerYourAppPage && !this.props.logOutPage ? (
+            <RegisterYourApp />
+          ) : null}
+          {this.props.logOutPage ? <LogOut /> : null}
         </AppBody>
       </AppWrap>
-    )
+    );
   }
 }
 
@@ -80,9 +66,9 @@ const mapStateToProps = (state, props) => {
     collectCredentialsPage: state.App.collectCredentialsPage,
     registerYourAppPage: state.App.registerYourAppPage,
     logOutPage: state.App.logOutPage
-  }
-}
-const mapDispatchToProps = (dispatch) => {
-  return { actions: bindActionCreators(AppActions, dispatch) }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return { actions: bindActionCreators(AppActions, dispatch) };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(App);
